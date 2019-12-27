9238345afdd88a78020c37956140fa5cfc8cc482
EC2 Default User <ec2-user@ip-172-31-19-21.ap-southeast-1.compute.internal>
2019-12-27 08:03:15 +0000

    lesson 7

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import './sign-in.styles.scss';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component{
   constructor(props){
@@ -15,12 +15,22 @@ class SignIn extends React.Component{
     }
   }
   
-  handleSubmit = e => {
-    e.preventDefault();    
-    this.setState({
-      email : '',
-      password : ''
-    })
+  handleSubmit = async e => {
+    e.preventDefault();
+    
+    const {email, password} = this.state;
+    
+    try{
+      await auth.signInWithEmailAndPassword(email, password);
+       this.setState({
+        email : '',
+        password : ''
+      })
+    }catch(err){
+      console.error(err);
+    }
+    
+   
   }
   
   handleChange = e => {
@@ -31,7 +41,7 @@ class SignIn extends React.Component{
   render(){
     return (
       <div className="sign-in">
-        <h2>I already have an account</h2>
+        <h2 className="title">I already have an account</h2>
         <span>Sign in with your email and password</span>
         
         <form onSubmit={this.handleSubmit}>
