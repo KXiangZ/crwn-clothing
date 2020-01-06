bdef1600e306e85f89a57749bbe42127ab49ddda
EC2 Default User <ec2-user@ip-172-31-19-21.ap-southeast-1.compute.internal>
2020-01-06 04:37:47 +0000

    lesson 11

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import './collection-item.styles.scss';
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.action';
 
-const CollectionItem = ({id , name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div style={{
-      backgroundImage: `url(${imageUrl})`
-    }} className="image" />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+import CustomButton from '../custom-button/custom-button.component';
+
+const CollectionItem = ({item, addItem }) => {
+  const {imageUrl, name, price} = item;
+  
+  return (
+    <div className="collection-item">
+      <div style={{
+        backgroundImage: `url(${imageUrl})`
+      }} className="image" />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton inverted onClick={() => addItem(item)}>Add to cart</CustomButton>
     </div>
-  </div>
-)
+  )
+}
 
-export default CollectionItem;
+
+
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item)) 
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
