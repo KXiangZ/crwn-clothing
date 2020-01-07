ed2cf4e1f66169c41153267c4a39150160bc152b
EC2 Default User <ec2-user@ip-172-31-19-21.ap-southeast-1.compute.internal>
2020-01-07 04:17:24 +0000

    lesson 14

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -4,10 +4,20 @@ const selectCart = state => state.cart;
 
 export const  selectCartItems = createSelector(
   [selectCart], 
-  (cart) => cart.cartItems
+  cart => cart.cartItems
+)
+
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden 
 )
 
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
   cartItems => cartItems.reduce((accumaltedQuantity, {quantity})=> accumaltedQuantity + quantity, 0)
+)
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems => cartItems.reduce((accumaltedTotal, {quantity, price})=> accumaltedTotal + quantity * price, 0)
 )
